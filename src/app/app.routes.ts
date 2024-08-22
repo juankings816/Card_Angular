d496bc002a31b34062ad3580b99c194fb9134555
Juan Reyes <juanmanuelreyesgarcia816@gmail.com>
2024-08-21 20:33:23 -0500

    Create app, firts component

---
+++ src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { Example1Component } from './components/example1/example1.component';
+
+export const routes: Routes = [
+    {
+        path:'',
+        component:Example1Component
+    }
+];
